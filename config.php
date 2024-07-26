3a058dbcdab7b892cee8e13790d9874b6f2ff49a
Spencer Lobo <139161179+spencermlobo@users.noreply.github.com>
2024-07-25 21:30:47 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "travelguide";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+$base_url = "https:/localhost/animated-epic-php-user-management-system-main/";
+$my_email = "omkarmoresrk13@gmail.com";
+
+?>
